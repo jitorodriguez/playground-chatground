ed9d4aecd81f6cb77b4797a76cff37651727c685
Jorge Nop <jito.rodriguez@outlook.com>
2019-12-15 12:38:38 -0500

    Successfully implement socket.io to listen over port. Connection event established.

--- server/server.js
+++ server/server.js
@@ -6,12 +6,27 @@ var app = express();
 app.use(express.static(__dirname));
 app.use(bodyParser.urlencoded({ extended: true}));
 
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+/*
 var server = app.listen(7000, () => {
     console.log("server is running on port", server.address().port);
 });
+*/
  
 app.post('/messages', (req, res) => {
     var message = req.body;
     console.log(message);
 });
+
+http.listen(7000, function(){
+    console.log('I hear you :)');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+});
+
+
 
